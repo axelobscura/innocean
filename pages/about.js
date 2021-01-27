3b840ab14e01c5130a564f9526a79fe712e489eb
Axel <axosar@gmail.com>
2021-01-27 10:00:36 -0600

    Textos quienes somos

--- pages/about.js
+++ pages/about.js
@@ -5,7 +5,7 @@ export default function About() {
       <div>
         <h2>INNOCEAN WORLDWIDE</h2>
         <p><b>INNOCEAN</b> Compañia global de comunicación y marketing con 18 operaciones en el mundo y más de 1,700 empleados.</p>
-        <p>En <b>INNOCEAN Worldwide México</b> creemos en proporcionar una gama completa de servicios con un enfoque claro para entregar valor a todos nuestros clientes.</p>
+        <p>En <b>INNOCEAN Worldwide México</b> creemos en proporcionar una gama completa de servicios<br/>con un enfoque claro para entregar valor a todos nuestros clientes.</p>
       </div>
       <div className="links">
         <Link href="/" as={process.env.BACKEND_URL + '/'}>
