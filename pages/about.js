c758d13a5933ffae165530388dbd2b94f1dfeaae
Axel <axosar@gmail.com>
2021-01-27 09:48:14 -0600

    About layout

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,14 @@
 import Link from 'next/link'
 export default function About() {
   return (
-    <div>
-      <div>About us</div>
+    <div className="about">
       <div>
-        Back to{' '}
+        <h2>INNOCEAN WORLDWIDE</h2>
+        <p>INNOCEAN Compañia global de comunicación y marketing con 18 operaciones en el mundo y más de 1,700 empleados.</p>
+      </div>
+      <div>
         <Link href="/" as={process.env.BACKEND_URL + '/'}>
-          <a>Home</a>
+          <a>HOME</a>
         </Link>
       </div>
     </div>
