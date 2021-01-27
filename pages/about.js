4d12f0af122b55442e867e44656ba022fa9838a1
Axel <axosar@gmail.com>
2021-01-27 09:57:44 -0600

    Textos

--- pages/about.js
+++ pages/about.js
@@ -4,11 +4,15 @@ export default function About() {
     <div className="about">
       <div>
         <h2>INNOCEAN WORLDWIDE</h2>
-        <p>INNOCEAN Compañia global de comunicación y marketing con 18 operaciones en el mundo y más de 1,700 empleados.</p>
+        <p><b>INNOCEAN</b> Compañia global de comunicación y marketing con 18 operaciones en el mundo y más de 1,700 empleados.</p>
+        <p>En <b>INNOCEAN Worldwide México</b> creemos en proporcionar una gama completa de servicios con un enfoque claro para entregar valor a todos nuestros clientes.</p>
       </div>
-      <div>
+      <div className="links">
         <Link href="/" as={process.env.BACKEND_URL + '/'}>
           <a>HOME</a>
+        </Link>
+        <Link href="/" as={process.env.BACKEND_URL + '/'}>
+          <a>SERVICIOS</a>
         </Link>
       </div>
     </div>
