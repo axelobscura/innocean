94d45c73ccb85360794d186ea9462a981e7f2616
Axel <axosar@gmail.com>
2021-01-27 09:18:54 -0600

    fondo

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,21 @@
+import Head from 'next/head'
 import Link from 'next/link'
+
 export default function Home() {
   return (
     <div>
-      Hello World.{' '}
-      <Link href="/about" as={process.env.BACKEND_URL + '/about'}>
-        <a>About</a>
-      </Link>
+      <Head>
+        <title>INNOCEAN MÉXICO</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+
+      <div className="example">
+        Hello World.{' '}
+        <Link href="/about" as={process.env.BACKEND_URL + '/about'}>
+          <a>About</a>
+        </Link>
+      </div>
     </div>
+    
   )
 }
